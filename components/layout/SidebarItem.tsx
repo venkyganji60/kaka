2b44f1ebbda49de05c928c42c3c2c61f51e21565
venkyganji60 <135597770+venkyganji60@users.noreply.github.com>
2023-12-14 13:36:04 +0530

    mra

--- components/layout/SidebarItem.tsx
+++ components/layout/SidebarItem.tsx
@@ -34,6 +34,23 @@ const SidebarItem: React.FC<SidebarItemProps> = ({
       >
         <Icon size={28} color="white" />
       </div>
+      <div
+        className=" 
+       relative
+        hidden 
+        lg:flex  
+        gap-4 
+        p-4 
+        rounded-full 
+        hover:bg-slate-300 
+        hover:bg-opacity-10 
+        cursor-pointer
+        items-center"
+      >
+        <Icon size={24} color="white" />
+
+        <p className="hidden lg:block text-red-950 text-xl">{label}</p>
+      </div>
     </div>
   );
 };
