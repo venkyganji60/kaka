61c6a24793573ba9defa20bca4cea1dabbfe6c96
venkyganji60 <135597770+venkyganji60@users.noreply.github.com>
2023-12-14 12:41:39 +0530

    mra

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,9 +1,47 @@
+//import { signOut } from 'next-auth/react';
+import { BiLogOut } from "react-icons/bi";
+import { BsHouseFill, BsBellFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
+import SidebarLogo from "./SidebarLogo";
+
+//import useCurrentUser from '@/hooks/useCurrentUser';
+
+//import SidebarItem from './SidebarItem';
+///import SidebarLogo from './SidebarLogo';
+//import SidebarTweetButton from './SidebarTweetButton';
+
 const Sidebar = () => {
-    return ( 
-        <div>
-            
+  const items = [
+    {
+      icon: BsHouseFill,
+      label: "Home",
+      href: "/",
+    },
+    {
+      icon: BsBellFill,
+      label: "Notifications",
+      href: "/notifications",
+      //auth: true,
+      // alert: currentUser?.hasNotification
+    },
+    {
+      icon: FaUser,
+      label: "Profile",
+      href: "/user/123",
+      //  href: `/users/${currentUser?.id}`,
+      // auth: true,
+    },
+  ];
+
+  return (
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+      <div className="flex flex-col items-end">
+        <div className="space-y-2 lg:w-[230px]">
+          <SidebarLogo />
         </div>
-     );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default Sidebar;
